f7b17d9901d7e3dd4ac4b3cfa9e9b3e48e524983
Douglas R Colkitt <douglas.colkitt@gmail.com>
2019-02-22 15:59:36 +0000

    Modify walkThru to pass through the any ingestion errors

--- lib/go/walk.go
+++ lib/go/walk.go
@@ -9,10 +9,8 @@ import (
 func walkThru (root string, hndl PathIngester) error {
 	return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 			return err
 		}
-		hndl.ingestPath(path, info)
-		return nil
+		return hndl.ingestPath(path, info)
 	})
 }
