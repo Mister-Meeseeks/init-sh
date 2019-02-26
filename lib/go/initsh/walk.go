bafb59583fd87c657e8a49b4fb01e278eea48138
Douglas R Colkitt <douglas.colkitt@gmail.com>
2019-02-26 04:51:08 +0000

    Walk ignores hidden files and tilde files

--- lib/go/initsh/walk.go
+++ lib/go/initsh/walk.go
@@ -3,6 +3,7 @@ package initsh
 import (
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func WalkThru (importArg string, dir ImportDirector) error {
@@ -17,7 +18,25 @@ func walkIngest (root string, hndl PathIngester) error {
 	return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
+		} else if isIgnorable (path) {
+			return filepath.SkipDir
+		} else {
+			return hndl.ingestPath(path, info)
 		}
-		return hndl.ingestPath(path, info)
 	})
+}
+
+func isIgnorable (path string) bool {
+	base := filepath.Base(path)
+	return isHiddenBase(base) || isScratchBase(base)
+}
+
+func isHiddenBase (path string) bool {
+	return strings.HasPrefix(path, ".") &&
+		path != "." && path != "./" &&
+		path != ".." && path != "../"
+}
+
+func isScratchBase (path string) bool {
+	return strings.HasSuffix(path, "~")
 }
